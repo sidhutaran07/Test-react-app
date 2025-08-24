ad78ed48dce801b2b0a9dd94f25a40693e4eb574
sidhutaran07 <taranpreets194@gmail.com>
2025-08-25 01:48:37 +0530

    Update AIToolpage.js

--- src/components/AIToolpage.js
+++ src/components/AIToolpage.js
@@ -23,7 +23,7 @@ const AIToolPage = () => {
 
     try {
       // Send a POST request to your n8n webhook URL
-      const response = await fetch("https://taranpreets.app.n8n.cloud/webhook/xoxwebhook", {
+      const response = await fetch("process.env.REACT_APP_N8N_WEBHOOK_URL", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
