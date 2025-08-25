950836011ba362b5ec5946910c4a091c17302cca
sidhutaran07 <taranpreets194@gmail.com>
2025-08-25 16:45:10 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AuthProvider } from './AuthContext'; // import your context
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
