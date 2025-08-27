3cab7bffbd71321cf1214e62653607a7027cc8a2
sidhutaran07 <taranpreets194@gmail.com>
2025-08-27 22:34:03 +0530

    Update LeadsPage.js

--- src/components/LeadsPage.js
+++ src/components/LeadsPage.js
@@ -1,6 +1,6 @@
 
 import { useState, useEffect } from "react";
-import { useAuth } from "../firebase";
+import { useAuth } from "../AuthContext";
 
 export default function Leads() {
   const { currentUser } = useAuth();
