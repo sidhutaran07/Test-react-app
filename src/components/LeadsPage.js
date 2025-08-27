f6c70986a11d07c2a47e0db3b65575365b3cd1d6
sidhutaran07 <taranpreets194@gmail.com>
2025-08-27 22:16:51 +0530

    Update LeadsPage.js

--- src/components/LeadsPage.js
+++ src/components/LeadsPage.js
@@ -1,6 +1,6 @@
 
 import { useState, useEffect } from "react";
-import { useAuth } from "../context/AuthContext";
+import { useAuth } from "./src/AuthContext";
 
 export default function Leads() {
   const { currentUser } = useAuth();
