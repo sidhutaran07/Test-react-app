172d61d655c9477efe1a157177ff5a2966b538f5
sidhutaran07 <taranpreets194@gmail.com>
2025-09-12 23:56:00 +0530

    Update Inventory.js

--- src/pages/Inventory.js
+++ src/pages/Inventory.js
@@ -1,9 +1,9 @@
 import { useEffect, useMemo, useState } from "react";
-import { Button } from "./components/ui/button";
-import { Input } from "./components/ui/input";
-import { Label } from "./components/ui/label";
-import { Card, CardContent, CardHeader } from "./components/ui/card";
-import { Table, THead, TBody, TR, TH, TD } from "./components/ui/table";
+import { Button } from "./components/ui/Button";
+import { Input } from "./components/ui/Input";
+import { Label } from "./components/ui/Label";
+import { Card, CardContent, CardHeader } from "./components/ui/Card";
+import { Table, THead, TBody, TR, TH, TD } from "./components/ui/Table";
 
 
 function RowEditor({ row, onChange, onRemove }) {
