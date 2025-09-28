ec5b36f784dcfe758da529d0db81a2c14d544a92
sidhutaran07 <taranpreets194@gmail.com>
2025-09-29 05:15:31 +0530

    Update Work.js

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -1,5 +1,5 @@
 import Soundcloudwidget from "../components/Soundcloudwidget";
-import DraggableWidgetContainer from "../components/DraggableWidgetContainer";
+import DraggableWidgetcontainer from "../components/DraggableWidgetcontainer";
 export default function  Work () {
   return(
 <section>
